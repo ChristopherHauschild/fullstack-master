6c310105ef877ff93b25765cbac7c5ffdc96ac44
ChristopherHauschild <chrishaus01@gmail.com>
2020-07-16 22:22:22 -0300

    Navegation finalized

--- M05/my-convertmymoney/index.js
+++ M05/my-convertmymoney/index.js
@@ -6,9 +6,21 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+// css | images | arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/quotation", (req, res) => {
+  const { by, from, quantity } = req.query;
+
+  res.render("quotation", {
+    by,
+    from,
+    quantity,
+  });
 });
 
 app.listen(port, (err) => {
